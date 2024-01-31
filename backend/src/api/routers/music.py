cacd6e78dcb72d64c8e1a871aa7fff8aedb40902
Grigory Malanchuk <grigorymalanchuk@gmail.com>
2024-01-31 15:44:15 +0200

    connected pytube to download a track from youtube

--- backend/src/api/routers/music.py
+++ backend/src/api/routers/music.py
@@ -1,28 +1,37 @@
 import spotipy
 from fastapi import APIRouter, Request
+from pytube import YouTube, Search
 
-from src.api.schemas.music import TrackInformationSchema, TrackReferenceSchema
+from src.api.schemas.music import TrackReferenceSchema
 
 
 music_router = APIRouter(prefix="/v1/music", tags=["Music"])
 
 
-@music_router.post(path="/", response_model=TrackInformationSchema)
+@music_router.post(path="/")
 async def get_information_about_track_by_reference(
-        track_reference: TrackReferenceSchema, request: Request
-) -> TrackInformationSchema:
+        spotify_track_reference_in_request: TrackReferenceSchema, request: Request
+) -> None:
+    # todo separate this code into a separate function
     access_token = request.session["access_token"]
     spotify_client = spotipy.Spotify(auth=access_token)
+    # todo -------------------------------------------
 
-    track_reference = str(track_reference.model_dump()["track_reference"])
-    information_about_track = spotify_client.track(track_reference)
+    spotify_track_reference = str(spotify_track_reference_in_request.model_dump()["track_reference"])
+    information_about_track = spotify_client.track(spotify_track_reference)
 
     track_name = information_about_track["name"]
-    track_artists = [artist["name"] for artist in information_about_track["artists"]]
-    track_cover_art_url = information_about_track["album"]["images"][0]["url"]
+    track_artists = ', '.join(
+        [artist["name"] for artist in information_about_track["artists"]]
+    )  # for a beautiful display of music authors, like: 'Justin Hurwitz, Li Jun Li'
 
-    return TrackInformationSchema(
-        track_name=track_name,
-        track_artists=track_artists,
-        track_cover_art_url=track_cover_art_url
-    )
+    youtube_track_reference = Search(f'{track_name} {track_artists}').results[0].watch_url
+
+    track = YouTube(youtube_track_reference)
+    stream = track.streams.filter(only_audio=True).first()
+    stream.download(filename=f"{track_artists} - {track_name}.mp3", output_path="music")
+
+    # TODO create storage for all mp3 files on DO Spaces and return to users only link,
+    #  which will start automatic download of file
+
+    return None
