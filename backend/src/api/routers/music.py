17631824c115460e845255d7b9e2165559540a43
Grigory Malanchuk <grigorymalanchuk@gmail.com>
2024-02-01 12:26:25 +0200

    connected and configured DO Spaces for track storage

--- backend/src/api/routers/music.py
+++ backend/src/api/routers/music.py
@@ -1,9 +1,11 @@
+import os
+
 import spotipy
 from fastapi import APIRouter, Request
 from pytube import YouTube, Search
 
 from src.api.schemas.music import TrackReferenceSchema
-
+from src.config import digital_ocean_spaces, settings
 
 music_router = APIRouter(prefix="/v1/music", tags=["Music"])
 
@@ -12,10 +14,10 @@ music_router = APIRouter(prefix="/v1/music", tags=["Music"])
 async def get_information_about_track_by_reference(
         spotify_track_reference_in_request: TrackReferenceSchema, request: Request
 ) -> None:
-    # todo separate this code into a separate function
+    # TODO separate this code into a separate function
     access_token = request.session["access_token"]
     spotify_client = spotipy.Spotify(auth=access_token)
-    # todo -------------------------------------------
+    # TODO -------------------------------------------
 
     spotify_track_reference = str(spotify_track_reference_in_request.model_dump()["track_reference"])
     information_about_track = spotify_client.track(spotify_track_reference)
@@ -24,14 +26,24 @@ async def get_information_about_track_by_reference(
     track_artists = ', '.join(
         [artist["name"] for artist in information_about_track["artists"]]
     )  # for a beautiful display of music authors, like: 'Justin Hurwitz, Li Jun Li'
+    track_filename = f"music/{track_artists} - {track_name}.mp3"  # track name and location on the server
 
     youtube_track_reference = Search(f'{track_name} {track_artists}').results[0].watch_url
 
+    # TODO create a separate thread for downloading music
     track = YouTube(youtube_track_reference)
     stream = track.streams.filter(only_audio=True).first()
-    stream.download(filename=f"{track_artists} - {track_name}.mp3", output_path="music")
+    stream.download(filename=track_filename)
+    # TODO ----------------------------------------------
 
-    # TODO create storage for all mp3 files on DO Spaces and return to users only link,
-    #  which will start automatic download of file
+    with open(track_filename, 'rb') as downloaded_track:
+        digital_ocean_spaces.upload_fileobj(
+            Fileobj=downloaded_track,  # track which will be uploaded to DO Spaces
+            Bucket='music',  # name of the directory in DO Spaces
+            Key=f'{track_artists} - {track_name}.mp3',  # name of the track
+            ExtraArgs={'ACL': 'public-read'},  # make the file public
+        )
+
+    os.remove(track_filename)  # remove the track from the server
 
     return None
--- backend/src/config.py
+++ backend/src/config.py
@@ -1,3 +1,4 @@
+import boto3
 from pydantic_settings import BaseSettings
 from spotipy import SpotifyOAuth
 
@@ -5,12 +6,17 @@ from spotipy import SpotifyOAuth
 class Settings(BaseSettings):
     """Loading environments from the .env file"""
 
+    SESSION_SECRET_KEY: str
+
     SPOTIFY_CLIENT_ID: str
     SPOTIFY_CLIENT_SECRET: str
     SPOTIFY_REDIRECT_URI: str
     SPOTIFY_SCOPE: str
 
-    SESSION_SECRET_KEY: str
+    DO_SPACES_REGION_NAME: str
+    DO_SPACES_ENDPOINT_URL: str
+    DO_SPACES_ACCESS_KEY: str
+    DO_SPACES_SECRET_KEY: str
 
     class Config:
         env_file = ".env"
@@ -25,4 +31,13 @@ spotify_oauth = SpotifyOAuth(
     client_secret=settings.SPOTIFY_CLIENT_SECRET,
     redirect_uri=settings.SPOTIFY_REDIRECT_URI,
     scope=settings.SPOTIFY_SCOPE,
+)
+
+# DIGITAL OCEAN SPACES
+digital_ocean_spaces = boto3.client(
+    "s3",
+    region_name=settings.DO_SPACES_REGION_NAME,
+    endpoint_url=settings.DO_SPACES_ENDPOINT_URL,
+    aws_access_key_id=settings.DO_SPACES_ACCESS_KEY,
+    aws_secret_access_key=settings.DO_SPACES_SECRET_KEY,
 )
