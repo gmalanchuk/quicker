00262cdafbf9173f5dfbdaf862219ae21927e9aa
Grigory Malanchuk <grigorymalanchuk@gmail.com>
2024-07-10 18:13:42 +0300

    Initial commit

---
+++ bot/src/config.py
@@ -0,0 +1,14 @@
+# from pydantic_settings import BaseSettings
+#
+#
+# class Settings(BaseSettings):
+#     """Loading environments from the .env file"""
+#
+#     BOT_TOKEN: str
+#
+#     class Config:
+#         env_file = ".env"
+#
+#
+# # ENVIRONMENT VARIABLES
+# settings = Settings()
